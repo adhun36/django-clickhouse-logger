9e25f200659c7c1f5f8b2d3f115b351e673f54d7
root <root@dsde586.fornex.org>
2022-08-28 19:42:08 +0000

    Sobolev:Init

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import setuptools
+
+__author__ = 'Sobolev Andrey <email.asobolev@gmail.com>'
+__version__ = '0.1.0'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-clickhouse-logger',
+    version=__version__,
+    install_requires=['clickhouse-driver==0.2.4'],
+    author='Sobolev Andrey',
+    author_email='email.asobolev@gmail.com',
+    description='Logging django errors to the clickhouse database.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Sobolev5/django-clickhouse-logger/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

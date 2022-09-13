3e0d58ab23457f94c8a592fc1c0ebf334f6ed49c
Andrey Sobolev <email.asobolev@gmail.com>
2022-09-13 15:10:14 +0000

    Sobolev:Fix

--- django_clickhouse_logger/handlers.py
+++ django_clickhouse_logger/handlers.py
@@ -8,6 +8,5 @@ from django.core.handlers.wsgi import WSGIRequest
 class ClickhouseLoggerHandler(StreamHandler):
 
     def emit(self, record) -> None:
-        if isinstance(record, logging.LogRecord) and getattr(record, 'request', False):
-            if isinstance(record.request, WSGIRequest):
-                clickhouse.proxy(record)
+        if isinstance(record, logging.LogRecord) and getattr(record, 'request', False) and isinstance(record.request, WSGIRequest):
+            clickhouse.proxy(record)
